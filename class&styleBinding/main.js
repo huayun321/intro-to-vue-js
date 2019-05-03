633dcd8c0060451b5ef3280dea52ba2144d52798
huayun321 <43416244@qq.com>
2019-05-03 16:51:01 +0800

    class style binding

---
+++ class&styleBinding/main.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./green.png",
+        altText: "A pair of socks",
+        inStock: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./green.png"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./blue.png"
+            }],
+        cart:0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            console.log(variantImage)
+            this.image = variantImage
+        }
+    }
+})
