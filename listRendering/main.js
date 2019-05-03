6ebe815de77b8baddc45ccdd7544275021e786eb
huayun321 <43416244@qq.com>
2019-05-03 16:22:34 +0800

    list rendering

---
+++ listRendering/main.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./green.png",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            },
+
+        ]
+    }
+})
